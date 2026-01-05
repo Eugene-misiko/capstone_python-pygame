72ad8c90bb0a7975fa50e0fe8d80c2f633711474
Eugene <eugenemisiko438@gmail.com>
2026-01-05 09:13:39 +0300

    added functionality using pygame logics, mixer, and font library

--- main.py
+++ main.py
@@ -1,26 +1,106 @@
 import pygame
 import os
-
+pygame.font.init()#Initializes font library
+pygame.mixer.init()
 WIN = pygame.display.set_mode((900,500))
 pygame.display.set_caption("First Game")
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+YELLOW = (255, 255, 0)
+BOARDER = pygame.Rect(900//2 - 5, 0, 10, 500)
+
+BULLET_HIT_SOUND = pygame.mixer.Sound(os.path.join('assets', 'Grenade+1.mp3'))
+BULLET_FIRE_SOUND = pygame.mixer.Sound(os.path.join('assets', 'Gun+Silencer.mp3'))
+
+HEALTH_FONT = pygame.font.SysFont('comicsans', 40)
+WINNER_FONT = pygame.font.SysFont('comicsans', 100)
+
 FPS = 60
 VEL = 5
+BULLET_VEL = 7
+MAX_BULLETS = 3
+
+YELLOW_HIT = pygame.USEREVENT + 1
+RED_HIT = pygame.USEREVENT + 2
 
 PLAYER1 = pygame.image.load(os.path.join('assets', 'spaceship_yellow.png'))
 PLAYER_1 = pygame.transform.rotate(pygame.transform.scale(PLAYER1, (55, 40)), 90)
 PLAYER2 = pygame.image.load(os.path.join('assets', 'spaceship_red.png'))
 PLAYER_2 = pygame.transform.rotate(pygame.transform.scale(PLAYER2, (55, 40)),270)
 
-def draw_window(red, yellow):
-    WIN.fill((255,255,255))
+SPACE = pygame.transform.scale(pygame.image.load(os.path.join('assets', 'space.png')),(900, 500))
+
+def draw_window(red, yellow, red_bullets, yellow_bullets, red_health, yellow_health):
+    WIN.blit(SPACE, (0, 0))
+    pygame.draw.rect(WIN, BLACK, BOARDER)
+
+    red_health_text = HEALTH_FONT.render("Health: " + str(red_health), 1, (255, 255, 255))
+    yellow_health_text = HEALTH_FONT.render("Health: " + str(yellow_health), 1, (255, 255, 255))
+    WIN.blit(red_health_text, (900 - red_health_text.get_width() - 10, 10))
+    WIN.blit(yellow_health_text, (10, 10))
     WIN.blit(PLAYER_1, (yellow.x, yellow.y))
     WIN.blit(PLAYER_2,(red.x, red.y))
-    pygame.display.update() 
 
+
+    for bullet in red_bullets:
+        pygame.draw.rect(WIN, RED, bullet)
+
+    for bullet in yellow_bullets:
+        pygame.draw.rect(WIN, YELLOW, bullet)        
+
+    pygame.display.update() 
+def yellow_handle_movement(keys_pressed,yellow):
+    if keys_pressed[pygame.K_a] and yellow.x - VEL > 0:#key left
+        yellow.x -= VEL
+    if keys_pressed[pygame.K_d] and yellow.x + VEL + yellow.width < BOARDER.x :#key right
+        yellow.x += VEL
+    if keys_pressed[pygame.K_w] and yellow.y - VEL > 0:#key up
+        yellow.y -= VEL
+    if keys_pressed[pygame.K_s] and yellow.y + VEL + yellow.height < 500 - 15:#key down
+        yellow.y += VEL
+
+def red_handle_movement(keys_pressed,red):
+    if keys_pressed[pygame.K_LEFT] and red.x - VEL > BOARDER.x + BOARDER.width:#key left
+        red.x -= VEL
+    if keys_pressed[pygame.K_RIGHT] and red.x + VEL + red.width < 900:#key right
+        red.x += VEL
+    if keys_pressed[pygame.K_UP] and red.y - VEL > 0:#key up
+        red.y -= VEL
+    if keys_pressed[pygame.K_DOWN] and red.y + VEL + red.height < 500 - 15:#key down
+        red.y += VEL        
+
+
+def handle_bullets(yellow_bullets, red_bullets, yellow, red):
+    for bullet in yellow_bullets:
+        bullet.x += BULLET_VEL
+        if red.colliderect(bullet):
+            pygame.event.post(pygame.event.Event(RED_HIT))
+            yellow_bullets.remove(bullet)
+        elif bullet.x > 900:
+            yellow_bullets.remove(bullet)   
+
+    for bullet in red_bullets:
+        bullet.x -= BULLET_VEL
+        if yellow.colliderect(bullet):
+            pygame.event.post(pygame.event.Event(YELLOW_HIT))
+            red_bullets.remove(bullet) 
+        elif bullet.x < 0:
+            red_bullets.remove(bullet)               
+def draw_winner(text):
+    draw_text = WINNER_FONT.render(text, 1, (255, 255, 255))
+    WIN.blit(draw_text, (900/2 - draw_text.get_width()/2, 500/2 - draw_text.get_height()/2))
+    pygame.display.update()
+    pygame.time.delay(5000)
 
 def main():
     red = pygame.Rect(700, 300, 55, 40)
     yellow = pygame.Rect(100, 300, 55, 40)
+
+    red_bullets = []
+    yellow_bullets = []
+
+    red_health = 10
+    yellow_health = 10
     clock = pygame.time.Clock()
     run = True
     while run:
@@ -28,18 +108,48 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
-        
+                pygame.quit()
+
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LCTRL and len(yellow_bullets) < MAX_BULLETS:
+                    bullet = pygame.Rect(yellow.x + yellow.width, yellow.y + yellow.height//2 - 2, 10, 5)
+                    yellow_bullets.append(bullet)
+                    BULLET_FIRE_SOUND.play()
+
+                if event.key == pygame.K_RCTRL and len(red_bullets) < MAX_BULLETS:
+                    bullet = pygame.Rect(red.x, red.y + red.height//2 - 2, 10, 5)
+                    red_bullets.append(bullet)
+                    BULLET_FIRE_SOUND.play()       
+
+            if event.type == RED_HIT:
+                red_health -=1
+                BULLET_HIT_SOUND.play()
+
+            if event.type == YELLOW_HIT:
+                yellow_health -= 1
+                BULLET_HIT_SOUND.play()
+        winner_text = ""        
+        if red_health <= 0:
+            winner_text = "Yellow wins!"
+
+        if yellow_health <= 0:
+            winner_text = "Red wins!" 
+
+        if winner_text != "":
+            draw_winner(winner_text)
+            break
+
         keys_pressed = pygame.key.get_pressed()
-        if keys_pressed[pygame.K_a]:#key left
-            yellow.x -= VEL
-        if keys_pressed[pygame.K_d]:#key right
-            yellow.x += VEL
+        yellow_handle_movement(keys_pressed, yellow)
+        red_handle_movement(keys_pressed, red)
+        
+        handle_bullets(yellow_bullets,red_bullets, yellow, red)
           
                         
                 
-        draw_window(red, yellow)      
+        draw_window(red, yellow, red_bullets, yellow_bullets, red_health, yellow_health)      
 
-    pygame.quit()  
+    main()
 
 if __name__ == "__main__":
     main()            
